0ca71c3dcad7b2f5b7e608875ce6820c44d40ed8
paulohfsantos <paulohfsantos1@gmail.com>
2023-10-02 06:49:54 -0300

    add: vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,26 @@ import utc from "dayjs/plugin/utc";
 import updateLocale from "dayjs/plugin/updateLocale";
 import weekOfYear from "dayjs/plugin/weekOfYear";
 
+import Toast, { POSITION } from "vue-toastification";
+import type { PluginOptions } from 'vue-toastification'
+import "vue-toastification/dist/index.css";
+
 import App from './App.vue'
 import router from './router'
 
+const options: PluginOptions = {
+  draggable: true,
+  position: "top-right" as POSITION,
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnHover: true,
+  hideProgressBar: false,
+  icon: true,
+}
+
 const app = createApp(App)
 
+app.use(Toast, options)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
